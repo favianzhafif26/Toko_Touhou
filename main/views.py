8bda43a18d9d83380629089f3d390ba234853832
favianzhafif26 <favianzhafif26@gmail.com>
2024-09-08 11:11:29 +0700

    rename

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.templatetags.static import static
+
 def show_main(request):
     context = {
         'nama' : 'Fumo Cirno V1.5 Touhou Project Plushie + Bonus Pin - Down Payment',
@@ -15,7 +17,8 @@ def show_main(request):
     Note,
     
     Untuk pembayaran via DP, silahkan pilih varian Down Payment. Setelah itu akan dikirim barang dummy berupa invoice atau yang lainnya. Jika barang sudah sampai di Indonesia akan dikontak melalui nomor HP yang digunakan ketika pre order dan melakukan pelunasan dengan membeli produk ini lagi dengan varian Pelunasan.''',
-        'harga': '500000'
+        'harga': '500000',
+        'image': static('gambar_untuk_tugas2_pbp.jpg'),
     }
 
     return render(request, "main.html", context)
