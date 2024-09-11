281e46f1c9c98f7c07f58c85676b0c56cbd1e722
favianzhafif26 <favianzhafif26@gmail.com>
2024-09-11 11:04:48 +0700

    changingf project name a bit

--- Toko ᗜˬᗜ/settings.py
+++ Toko ᗜˬᗜ/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-zz7xpbzg+g@u-7ezz@zo^)!hm1p_e%)^j(8nsqdg2h$by4dd^t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "favian-zhafif-TokoTouhou.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "favian-zhafif-tokotouhou.pbp.cs.ui.ac.id"]
 
 
 # Application definition
