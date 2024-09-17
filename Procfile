f1b59e8bb338a3ab0ae5d5c13de0adcd8322aa19
favianzhafif26 <favianzhafif26@gmail.com>
2024-09-17 13:50:36 +0700

    fix: change project name to tokotouhou

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python3 manage.py migrate --noinput
-web: gunicorn Toko ᗜˬᗜ.wsgi
+web: gunicorn tokotouhou.wsgi
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Toko ᗜˬᗜ.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tokotouhou.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
